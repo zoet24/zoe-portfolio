ba571b925f84528c939b4c6fc3c529c695d518bf
zoet24 <zt12542@my.bristol.ac.uk>
2021-07-23 13:24:36 +0000

    Link index buttons to new pages

--- app.py
+++ app.py
@@ -17,6 +17,16 @@ def index():
     return render_template("pages/index/index.html")
 
 
+@app.route("/projects")
+def projects():
+    return render_template("pages/projects/projects_all/projects_all.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("pages/contact/contact.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
