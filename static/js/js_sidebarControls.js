269a4767a756538173ebfd49558df1785b6d0354
zoet24 <zt12542@my.bristol.ac.uk>
2021-07-24 23:19:20 +0000

    Add CSS to sidebar

--- static/js/js_sidebarControls.js
+++ static/js/js_sidebarControls.js
@@ -3,9 +3,11 @@ let sidebarOpen = true;
 function sidebarToggle() {
     if (sidebarOpen == true) {
         document.getElementById("sidebar").style.left = "-400px";
+        document.getElementById("sidebar-toggle-arrow").style.transform = "rotate(180deg)";
         sidebarOpen = false;
     } else if (sidebarOpen == false) {
         document.getElementById("sidebar").style.left = "0px";
+        document.getElementById("sidebar-toggle-arrow").style.transform = "rotate(0deg)";
         sidebarOpen = true;
     }
 }
